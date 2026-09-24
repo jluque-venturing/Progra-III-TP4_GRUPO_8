440bd81df71e3c39e1d1ee676c02c09e2fd440b4
Miguel Salazar <yobebe@hotmail.com.ar>
2026-09-24 19:23:22 -0300

    feat: add SQL operators to product and category filters. #2

--- TP4_GRUPO_8/ejercicio2.aspx.cs
+++ TP4_GRUPO_8/ejercicio2.aspx.cs
@@ -13,13 +13,28 @@ namespace TP4_GRUPO_8
         {
             if (!IsPostBack)
             {
+                CargarOperadores();
                 LimpiarFiltros();
             }
+        }
+
+        void CargarOperadores()
+        {
+            // Para los operadores de comparación, se agregan los elementos al DropDownList correspondiente
+            ddlOpProducto.Items.Add(new ListItem("Igual a:", "="));
+            ddlOpProducto.Items.Add(new ListItem("Mayor a:", ">"));
+            ddlOpProducto.Items.Add(new ListItem("Menor a:", "<"));
+
+            ddlOpCategoria.Items.Add(new ListItem("Igual a:", "="));
+            ddlOpCategoria.Items.Add(new ListItem("Mayor a:", ">"));
+            ddlOpCategoria.Items.Add(new ListItem("Menor a:", "<"));
         }
 
         void LimpiarFiltros()
         {
             txtIdProducto.Text = string.Empty;
+            ddlOpProducto.SelectedIndex = 0;
+            ddlOpCategoria.SelectedIndex = 0;
         }
     }
 }
